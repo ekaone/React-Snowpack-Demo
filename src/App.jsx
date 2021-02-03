4cb31c95ad3023354e451e862a9883d154e07f1c
ekaone <ekaone3033@gmail.com>
2021-02-03 10:32:46 +0700

    add tailwind min cdn

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
-import logo from './logo.svg'
+import React, { useState, useEffect } from "react";
+import naruto from "animexyz";
+import { Button } from "./components";
+
+naruto({ duration: 2000 });
 
 function App() {
   const [count, setCount] = useState(0);
+
   useEffect(() => {
     const timer = setTimeout(() => setCount(count + 1), 1000);
     return () => clearTimeout(timer);
   }, [count, setCount]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Page has been open for <code>{count}</code> seconds
+    <div>
+      <header>
+        <p className="text-red-700 uppercase">
+          Page has been open for <code>{count}</code> seconds..
         </p>
+        <hr />
+        <Button />
       </header>
     </div>
   );
---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Button() {
+  return (
+    <div>
+      <button className="bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 rounded-md">
+        Button
+      </button>
+    </div>
+  );
+}
+
+export default Button;
